37918dd8be9d6433ac08887f5677bb6f15eb8b42
Slevin <maglor@i.ua>
2011-12-25 03:33:57 +0200

    -upd passing original error code and string to Fw_Exception_Db_Query
    Signed-off-by: Slevin <maglor@i.ua>

--- Fw/Db/db.Query.php
+++ Fw/Db/db.Query.php
@@ -243,9 +243,9 @@ class Fw_Db_Query {
 
 	public function fetch() {
 		if (!$this->_execute()) {
-			$message = $this->_Stmt->errorInfo();
-			$code = $this->_Stmt->errorCode();
-			throw new Fw_Exception_Db_Query($message, $code);
+			$error = $this->_Stmt->errorInfo();
+			$code = (int)$this->_Stmt->errorCode();
+			throw new Fw_Exception_Db_Query($message[2], $code);
 		}
 
 		if ($this->getBehaviour() instanceof Fw_Db_Query_Behaviour_Insert) {
