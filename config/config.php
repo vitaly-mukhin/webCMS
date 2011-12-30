cdd73e0322d9a8f207486d705233acabc4d0a9ee
Vitaliy Mukhin <Vitaliy_Mukhin@epam.com>
2011-12-30 14:47:00 +0300

    -fix using sakila test DB also for production

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
 
 return array(
-	'db' => array(
-		'driver' => 'mysql',
-		'server' => 'localhost',
-		'port' => '3306',
-		'name' => 'sakila',
-		'user' => 'root',
-		'password' => 'mariadb',
-		'encoding' => 'utf8'
-	)
+		'db' => array(
+				'driver' => 'mysql',
+				'server' => 'localhost',
+				'port' => '3306',
+				'name' => 'sakila',
+				'user' => 'root',
+				'password' => 'mariadb',
+				'encoding' => 'utf8'
+		)
 );
