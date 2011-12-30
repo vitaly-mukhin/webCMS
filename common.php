c675b77585ade5c196fddaf153d6d6a2300f3d4e
Vitaliy Mukhin <Vitaliy_Mukhin@epam.com>
2011-12-30 14:45:46 +0300

    -fix PATH_FW folder

--- common.php
+++ common.php
@@ -5,16 +5,16 @@ define('TBL_FILM', 'film');
 define('TBL_LOG', 'log');
 
 define('PATH_CONFIG', PATH_ROOT . DIRECTORY_SEPARATOR . 'config');
-define('PATH_FW', PATH_ROOT . DIRECTORY_SEPARATOR . 'Library');
+define('PATH_FW', PATH_ROOT . DIRECTORY_SEPARATOR . 'Fw');
 
 require(PATH_FW . DIRECTORY_SEPARATOR . 'Autoloader.php');
 
 Autoloader::i()
-		->pushAutoload('Fw', PATH_FW . DIRECTORY_SEPARATOR, 'fw');
+	->pushAutoload('Fw', PATH_FW . DIRECTORY_SEPARATOR, 'fw');
 
 $Config = new Fw_Config(PATH_CONFIG . DIRECTORY_SEPARATOR . 'config.php');
 
 
 $Db = Fw_Db::i()->connect($Config->db);
 
-$id = $Db->query()->insert(TBL_LOG, array('sql'=>'aaaaaaaaa', 'result'=>'empty', 'duration'=>5))->fetchRow();
+$id = $Db->query()->insert(TBL_LOG, array('sql' => 'aaaaaaaaa', 'result' => 'empty', 'duration' => 5))->fetchRow();
