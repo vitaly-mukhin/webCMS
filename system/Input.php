10188d98ab1612683d901a0f0f436f7bd30b49a8
Vitaly Mukhin <vitaly.mukhin@gmail.com>
2012-04-29 23:51:18 +0300

    -add TestCase for a Input
    Signed-off-by: Vitaly Mukhin <vitaly.mukhin@gmail.com>

--- system/Input.php
+++ system/Input.php
@@ -14,28 +14,27 @@ class Input implements Input_I {
 	protected $data;
 
 	/**
+	 * Throws Exception if data are incompatible
 	 *
 	 * @param array $data 
 	 */
 	public function __construct($data) {
+		if (!is_array($data)) {
+			throw new ErrorException('Source data are incompatible');
+		}
+
 		$this->data = $data;
 	}
 
 	/**
 	 * Get a value from a holded data array.
-	 * Throws Exception if data are incompatible
 	 *
 	 * @param mixed $key
 	 * @param mixed $default
 	 * @return mixed
-	 * @throws Exception 
 	 */
 	public function get($key, $default = null) {
-		if (is_array($this->data)) {
-			return key_exists($key, $this->data) ? $this->data[$key] : $default;
-		}
-
-		throw new Exception('Source data are incompatible');
+		return key_exists($key, $this->data) ? $this->data[$key] : $default;
 	}
 
 }
