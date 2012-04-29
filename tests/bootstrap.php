8272cb28c549e95826b182e1eefc18b18a99c7ec
Vitaly Mukhin <vitaly.mukhin@gmail.com>
2012-04-29 23:30:30 +0300

    -add PHPUnit tests -add TestCase for a Router
    Signed-off-by: Vitaly Mukhin <vitaly.mukhin@gmail.com>

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+define('PATH_ROOT', __DIR__ . DIRECTORY_SEPARATOR . '..');
+define('PATH_BOOTSTRAP', PATH_ROOT . DIRECTORY_SEPARATOR . 'bootstrap');
+
+require PATH_BOOTSTRAP . DIRECTORY_SEPARATOR . 'common.php';
