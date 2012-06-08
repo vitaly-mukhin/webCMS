69192e8fd52c5ebc7cc1fb5680d714d9d62f8f8f
Vitaliy Mukhin <Vitaliy_Mukhin@epam.com>
2012-06-08 18:41:12 +0300

    -fix ajax-loading of reg form
    Signed-off-by: Vitaliy Mukhin <Vitaliy_Mukhin@epam.com>

--- www/public_html/js/block/auth.js
+++ www/public_html/js/block/auth.js
@@ -1,10 +1,10 @@
 
-$(document).ready(function(){
+$(document).ready(function(){	
 	$('a.auth-register').click(function(){
 		$.get(this.baseURI + 'block' + this.pathname, {}, function(data){
 			$('#body-content').html(data);
 		}, 'html');
-		document.location.hash = this.pathname;
+//		document.location.hash = this.pathname.substr(1);
 		return false;
 	});
 });
---
+++ www/public_html/js/block/auth/reg.js
@@ -0,0 +1,52 @@
+(function(){
+	var STATUS = {
+		OK: 'success',
+		FAILED: 'error'
+	};
+	
+	// Check for login field
+	$('#form-auth-reg input[name=login]').change(function(){
+		var s = true;
+		s = s && (this.value != '');
+		s = s && !(this.value.match(/[^$A-Za-z0-9_\-.]/i));
+		setStatus($(this), (s ? STATUS.OK : STATUS.FAILED));
+	});
+	
+	$('#form-auth-reg input[name=password], #form-auth-reg input[name=password_repeat]').change(function(){
+		var s = true;
+		s = s && this.value != '';
+		s = s && this.value.length >= 6;
+		setStatus($(this), (s ? STATUS.OK : STATUS.FAILED));
+	});
+
+	// Submitting the reg form
+	$('#form-auth-reg').submit(function(){
+		var result = true;
+			
+		var $login = $('input[name=login]', this);
+		$login.trigger('change');
+
+		// check Password and Password_Repeat
+		var $pwd = $('input[name=password]', this);
+		$pwd.trigger('change');
+
+		var $pwd_r = $('input[name=password_repeat]', this);
+		$pwd_r.trigger('change');
+			
+		if ($pwd.val() != $pwd_r.val()) {
+			result = false;
+			setStatus($pwd_r, STATUS.FAILED);
+		}
+			
+		result = !$('.control-group.' + STATUS.FAILED, this).get();
+		return result;
+	});
+
+	function setStatus($el, status) {
+		var $parent = $el.parents('.control-group');
+		for (var i in STATUS) {
+			$parent.removeClass(STATUS[i]);
+		}
+		$parent.addClass(status);
+	}
+})();
