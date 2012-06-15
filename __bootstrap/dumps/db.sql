37a5486b20b01d9ca83faaf7ba2c6710a38cc989
Vitaliy Mukhin <Vitaliy_Mukhin@epam.com>
2012-06-15 17:47:43 +0300

    -upd mysqldump
    Signed-off-by: Vitaliy Mukhin <Vitaliy_Mukhin@epam.com>

--- __bootstrap/dumps/db.sql
+++ __bootstrap/dumps/db.sql
@@ -21,14 +21,19 @@ USE `webcms`;
 DROP TABLE IF EXISTS `user_auths`;
 
 CREATE TABLE `user_auths` (
-  `user_auth_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `auth_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
   `login` varchar(54) NOT NULL,
   `hash` char(40) NOT NULL,
-  PRIMARY KEY (`user_auth_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`auth_id`),
+  UNIQUE KEY `login` (`login`),
+  KEY `FK_user_auths_user_id` (`user_id`),
+  CONSTRAINT `FK_user_auths_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user_auths` */
+
+insert  into `user_auths`(`auth_id`,`user_id`,`login`,`hash`) values (1,1,'someLogin','d763bba346510be03d985770878b157f8a382fcb'),(4,4,'someLogin1','343eafb0124d884a35cc8a8dd2df891a11558c86');
 
 /*Table structure for table `users` */
 
@@ -38,10 +43,13 @@ CREATE TABLE `users` (
   `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(54) NOT NULL,
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 /*Data for the table `users` */
+
+insert  into `users`(`user_id`,`email`,`date_created`,`username`) values (1,'example@example.com','2012-06-14 11:48:39','Ahaha!'),(4,'example@example.com','2012-06-15 16:37:13','User Userovich 2');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
