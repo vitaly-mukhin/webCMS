0079be49539f78a0b917a28256ee5025ffaa24de
Vitaliy Mukhin <v.mukhin@uni-tech.com.ua>
2012-08-09 18:22:14 +0300

    -upd add some functionality to login js
    Signed-off-by: Vitaliy Mukhin <v.mukhin@uni-tech.com.ua>

--- www/public_html/js/block/auth/login.js
+++ www/public_html/js/block/auth/login.js
@@ -1,6 +1,9 @@
-(function(){
-	$('#form-auth-login').submit(function(){
-		$.alert('Hello, world!');
-		return false;
+$(document).ready(function(){
+	$('.form-auth-login').submit(function(){
+		$.post(this.baseURI + 'json/' + 'block' + this.action.substr(this.baseURI.length-1), $(this).serialize(), function(data){
+                console.log(data);
+                $('#top-user').html($('<div/>').html(data.content).text());
+                appendScripts(data);
+			}, 'json');
 	});
 })
