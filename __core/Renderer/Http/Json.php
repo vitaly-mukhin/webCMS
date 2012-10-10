438b795d711e65f9a20df6dd9120b974bd4d2187
Vitaliy Mukhin <v.mukhin@uni-tech.com.ua>
2012-10-10 16:13:15 +0300

    -upd some changes

--- __core/Renderer/Http/Json.php
+++ __core/Renderer/Http/Json.php
@@ -17,12 +17,14 @@ class Renderer_Http_Json extends Renderer_Http {
         /* @var $Output Output_Http */
         $Output->header('Content-Type: text/json');
         $content = parent::render($Output, $templatePath);
+        $titles = Block_Head::getPageTitle();
 
         $result = $this->renderInner(new Output(array(
                     'head' => json_encode(array(
                         'script' => array_values(Block_Head::getJsLinks()),
                         'css' => array_values(Block_Head::getCssLinks())
                     )),
+                    'title' => json_encode(htmlspecialchars(reset($titles), ENT_NOQUOTES)),
                     'content' => json_encode(htmlspecialchars($content, ENT_NOQUOTES))
                 )), static::TPL);
 
--- www/public_html/js/block/auth.js
+++ www/public_html/js/block/auth.js
@@ -1,10 +1,7 @@
 
 $(document).ready(function(){	
-    $('a.auth-register, a.auth-reset').click(function(){
-        $.get(this.baseURI + 'json/' + 'block' + this.pathname, {}, function(data){
-            $('#body-content').html($('<div/>').html(data.content).text());
-            appendScripts(data);
-        }, 'json');
-        return false;
-    });
+//    $('a.auth-register, a.auth-reset').click(function(){
+//        P.load(this.baseURI + 'json/' + 'block' + this.pathname);
+//        return false;
+//    });
 });
