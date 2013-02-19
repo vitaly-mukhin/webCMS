9b3d50c614eb721eff1f7b901c800aa881003bde
Vitaliy Mukhin <vitaly.mukhin@gmail.com>
2013-02-19 12:43:35 +0200

    -fix Session for \Core\Model\Get

--- __core/Session.php
+++ __core/Session.php
@@ -6,79 +6,82 @@
  * @author Vitaliy_Mukhin
  */
 namespace Core;
+
+use Core\Model\Get;
+
 class Session {
 
-	use \Core\Model\Get;
+    use Get;
 
-	const USER = 'section_user';
+    const USER = 'section_user';
 
-	/**
-	 *
-	 * @var Session
-	 */
-	private static $i;
+    /**
+     *
+     * @var Session
+     */
+    private static $i;
 
-	protected $id;
+    protected $id;
 
-	protected function __construct(array $data, $sessionId) {
-		$this->traitSetData($data);
-		$this->id = $sessionId;
-	}
+    protected function __construct(array $data, $sessionId) {
+        $this->traitSetData($data);
+        $this->id = $sessionId;
+    }
 
-	/**
-	 *
-	 * @return Session
-	 * @throws Exception
-	 */
-	public static function i() {
-		if (!empty(self::$i)) {
-			return self::$i;
-		}
+    /**
+     *
+     * @return Session
+     * @throws \Exception
+     */
+    public static function i() {
+        if (!empty(self::$i)) {
+            return self::$i;
+        }
 
-		if (!session_start()) {
-			throw new Exception('Cannot start a session');
-		}
+        if (!session_start()) {
+            throw new \Exception('Cannot start a session');
+        }
 
-		$sessionId = session_id();
+        $sessionId = session_id();
 
-		$Session = new Session($_SESSION, $sessionId);
+        $Session = new Session($_SESSION, $sessionId);
 
-		return self::$i = $Session;
-	}
+        return self::$i = $Session;
+    }
 
-	public function __destruct() {
-		$_SESSION = $this->getArrayCopy();
-	}
+    public function __destruct() {
+        $_SESSION = $this->data;
+    }
 
-	/**
-	 *
-	 * @param string $sectionId
-	 *
-	 * @return Input
-	 */
-	public function get($sectionId) {
-		$data = $this->offsetExists($sectionId) ? $this->offsetGet($sectionId) : null;
+    /**
+     *
+     * @param string $sectionId
+     *
+     * @return Input
+     */
+    public function get($sectionId) {
+        $data = $this->traitGetter($sectionId);
 
-		return is_array($data) ? new Input($data) : $data;
-	}
+        return is_array($data) ? new Input($data) : $data;
+    }
 
-	/**
-	 *
-	 * @param string $sectionId
-	 * @param mixed  $params
-	 *
-	 * @return Session
-	 */
-	public function set($sectionId, $params = array()) {
+    /**
+     *
+     * @param string $sectionId
+     * @param mixed  $params
+     *
+     * @return Session
+     */
+    public function set($sectionId, $params = array()) {
+        if ($this->traitIsset($sectionId) && is_array($params)) {
+            $existing = $this->get($sectionId);
+            $params = (is_array($existing)) ? array_merge($existing, $params) : $params;
+        }
 
-		if ($this->offsetExists($sectionId) && is_array($params)) {
-			$existing = $this->offsetGet($sectionId);
-			$params   = (is_array($existing)) ? array_merge($existing, $params) : $params;
-		}
+        $this->data[$sectionId] = $params;
 
-		$this->offsetSet($sectionId, $params);
+        $this->traitSetData($this->data);
 
-		return $this;
-	}
-
+        return $this;
+    }
 }
