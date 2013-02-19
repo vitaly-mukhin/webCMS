649c6d1e0d16da0301d40c6c45ca1bd0c40dc689
Vitaliy Mukhin <vitaly.mukhin@gmail.com>
2013-02-19 12:44:54 +0200

    -fix App\Album for \Core\Model\Get

--- www/php/model/Album.php
+++ www/php/model/Album.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Core\Input as Input;
-use Core\Model\Get as Data;
+use Core\Model\Get as Get;
 use Core\Result as Result;
 use Core\User as User;
 use Fw_Db;
@@ -22,118 +22,117 @@ use Fw_Db;
  */
 class Album {
 
-	use Get;
+    use Get;
 
-	const TBL = 'albums';
+    const TBL = 'albums';
 
-	const LATEST_N = 10;
+    const LATEST_N = 10;
 
-	protected static $tblFields = array('album_id', 'title', 'date_created', 'date_modified', 'user_id');
+    protected static $tblFields = array('album_id', 'title', 'date_created', 'date_modified', 'user_id');
 
-	public function __construct($data) {
-		$this->traitSetData($data);
-	}
+    public function __construct($data) {
+        $this->traitSetData($data);
+    }
 
-	/**
-	 *
-	 * @param \Core\Input $data
-	 *
-	 * @return Result
-	 */
-	public static function add(Input $data) {
-		if (($Result = static::validate($data)) && $Result->error) {
-			return $Result;
-		}
+    /**
+     *
+     * @param \Core\Input $data
+     *
+     * @return Result
+     */
+    public static function add(Input $data) {
+        if (($Result = static::validate($data)) && $Result->error) {
+            return $Result;
+        }
 
-		if ($id = self::_add($Result->value)) {
-			return new Result($id);
-		}
+        if ($id = self::_add($Result->value)) {
+            return new Result($id);
+        }
 
-		return new Result(null, 'Виникла помилка. Спробуйте, будь ласка, пізніше');
-	}
+        return new Result(null, 'Виникла помилка. Спробуйте, будь ласка, пізніше');
+    }
 
-	/**
-	 *
-	 * @param Input $Data
-	 *
-	 * @return Result
-	 */
-	protected static function validate(Input $Data) {
-		if (!($title = $Data->get('title')) || !trim($title)) {
-			return new Result(null, array('Поле "Назва" пусте'));
-		}
+    /**
+     *
+     * @param Input $Data
+     *
+     * @return Result
+     */
+    protected static function validate(Input $Data) {
+        if (!($title = $Data->get('title')) || !trim($title)) {
+            return new Result(null, array('Поле "Назва" пусте'));
+        }
 
-		return new Result($Data);
-	}
+        return new Result($Data);
+    }
 
-	protected static function _add(Input $Data) {
-		$values = array(
-			'title'   => trim($Data->get('title', '')),
-			'user_id' => User::curr()->getUserId(),
-		);
+    protected static function _add(Input $Data) {
+        $values = array(
+            'title' => trim($Data->get('title', '')),
+            'user_id' => User::curr()->getUserId(),
+        );
 
-		$id = Fw_Db::i()->query()->insert(self::TBL, $values)->fetchRow();
+        $id = Fw_Db::i()->query()->insert(self::TBL, $values)->fetchRow();
 
-		return $id;
-	}
+        return $id;
+    }
 
-	public static function getLatest($n = self::LATEST_N) {
-		return self::getList($n);
-	}
+    public static function getLatest($n = self::LATEST_N) {
+        return self::getList($n);
+    }
 
-	/**
-	 *
-	 * @param int   $n
-	 * @param array $params
-	 *
-	 * @return array
-	 */
-	protected static function getList($n = self::LATEST_N, $params = array()) {
-		$db = Fw_Db::i();
-		$q  = $db->query()->select()->from(self::TBL, self::$tblFields)->limit($n)->orderBy(array('date_created' => false));
-		if (!empty($params['__where__'])) {
-			foreach ($params['__where__'] as $where => $value) {
-				$q->where($where, $value);
-			}
-		}
-		$list = $q->fetch();
+    /**
+     *
+     * @param int   $n
+     * @param array $params
+     *
+     * @return array
+     */
+    protected static function getList($n = self::LATEST_N, $params = array()) {
+        $db = Fw_Db::i();
+        $q = $db->query()->select()->from(self::TBL, self::$tblFields)->limit($n)->orderBy(array('date_created' => false));
+        if (!empty($params['__where__'])) {
+            foreach ($params['__where__'] as $where => $value) {
+                $q->where($where, $value);
+            }
+        }
+        $list = $q->fetch();
 
-		$result = array();
-		foreach ($list as $row) {
-			$result[] = new static($row);
-		}
+        $result = array();
+        foreach ($list as $row) {
+            $result[] = new static($row);
+        }
 
-		return $result;
-	}
+        return $result;
+    }
 
-	public static function getOwn($n = self::LATEST_N) {
-		return self::getList($n, array('__where__' => array('user_id = ?' => User::curr()->getUserId())));
-	}
+    public static function getOwn($n = self::LATEST_N) {
+        return self::getList($n, array('__where__' => array('user_id = ?' => User::curr()->getUserId())));
+    }
 
-	/***********************************************/
+    /***********************************************/
 
-	/**
-	 *
-	 * @param int $id
-	 *
-	 * @return self|null
-	 */
-	public static function getById($id) {
-		$row = Fw_Db::i()->query()->select()->from(self::TBL, self::$tblFields)->where('album_id = ?', $id)->fetchRow();
+    /**
+     *
+     * @param int $id
+     *
+     * @return self|null
+     */
+    public static function getById($id) {
+        $row = Fw_Db::i()->query()->select()->from(self::TBL, self::$tblFields)->where('album_id = ?', $id)->fetchRow();
 
-		if ($row) {
-			return new static($row);
-		}
+        if ($row) {
+            return new static($row);
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public function __get($name) {
-		return $this->traitGetter($name);
-	}
+    public function __get($name) {
+        return $this->traitGetter($name);
+    }
 
-	public function __isset($name) {
-		return $this->traitIsset($name);
-	}
-
+    public function __isset($name) {
+        return $this->traitIsset($name);
+    }
 }
